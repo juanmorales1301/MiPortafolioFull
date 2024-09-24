f0e7568f2bee9e3dbd696c429c2e5f4423812d8a
Juan Morales <jmorales@utriasystems.com>
2024-09-23 20:32:47 -0500

    Ajustes de rutas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,14 +4,15 @@ const path = require('path');
 
 const router = express.Router();
 
-//Rutas estaticas WEB
-router.use('/', express.static(path.join(__dirname, '../../front/MiPortafolio'))); //Index Portafolio
-router.use('/galeria', express.static(path.join(__dirname, '../../front/v-portafolio/dist'))); //Compilado VUE
-
 
 //Rutas modulares de API
 // router.use(process.env.RUTA_INIT + `/core`, require('./modules/core')); //Core
 // router.use(process.env.RUTA_INIT + `/auth`, require('./modules/auth')); //Autentificación
 router.use(process.env.RUTA_INIT + `/admin`, require('./modules/administracion/1.admin.route')); //Administración
+
+
+//Rutas estaticas WEB
+router.use('/', express.static(path.join(__dirname, '../../front/MiPortafolio'))); //Index Portafolio
+router.use('/galeria', express.static(path.join(__dirname, '../../front/v-portafolio/dist'))); //Compilado VUE
 
 module.exports = router;

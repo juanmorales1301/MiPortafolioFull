3e53684df055539253debc41acf08cbcdb3c5fb7
Juan Morales <jmorales@utriasystems.com>
2024-09-23 20:43:21 -0500

    Ajustes de rutas

--- src/controller/modules/contacto/contacto.control.js
+++ src/controller/modules/contacto/contacto.control.js
@@ -16,7 +16,6 @@ const ContactoControl = {
             const ipCliente = req.ip || req.headers['x-forwarded-for'] || req.connection.remoteAddress;
             const fechaContacto = new Date();
 
-            res.json(req.body);
             let con = await conexionModel.getConnection(); // Conecta a la instancia de BD
 
 

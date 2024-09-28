408a41186034389267c5e24eb4cc1d89fa5f9d31
Juan Morales <jmorales@utriasystems.com>
2024-09-27 21:36:11 -0500

    Integración de modales y ajuste de carga

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -15,5 +15,8 @@ router.use(process.env.RUTA_INIT + `/galeria`, require('./modules/galeria/1.gale
 //Rutas estaticas WEB
 router.use('/', express.static(path.join(__dirname, '../../front/MiPortafolio'))); //Index Portafolio
 router.use('/galeria', express.static(path.join(__dirname, '../../front/v-portafolio/dist'))); //Compilado VUE
+router.get('/galeria/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../front/v-portafolio/dist', 'index.html'));
+});
 
 module.exports = router;
